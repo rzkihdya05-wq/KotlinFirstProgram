39ba12af87836c9b08c7cb154b5b12ee8d893045
rizki hidaya <rzkihdya05@gmail.com>
2025-09-09 20:55:26 +0700

    Literal kotlin

---
+++ src/LiteralkKotlin.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+    println("intLiteral = " + intLiteral)
+    println("doubleLiteral = " + doubleLiteral)
+    println("stringLitera = " + stringLiteral)
+    println("charLiteral = " + charLiteral)
+    println("booLiteral = " + boolLiteral)
+}
