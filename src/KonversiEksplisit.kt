f6fdfd410ac7749f966da2c7fdc1480db6e7ef1f
rizki hidaya <rzkihdya05@gmail.com>
2025-09-15 00:01:33 +0700

    Penggunaan arrayOf() untuk membuat array

--- src/KonversiEksplisit.kt
+++ src/KonversiEksplisit.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
-    val valInt: Int = 1
+    var valInt: Int = 1
     val valSum: Long = 3L + valInt //konversi implisit
     println("Konversi Variabel valInt secara impilisit : " + valSum)
     val valLong: Long = valInt.toLong()//konversi eksplisit
     println("konversi variabel valInt secara eksplisit : " + valLong)
+
+    var varLong : Long = 3_000_000_000
+    valInt = valSum.toInt()
+    println(valInt)
 }
